f1e0236080aec0a39cba3dc639b8907031e455aa
장규희 <jang-gyuhui@jang-gyuhuis-MacBook-Pro.local>
2020-04-22 20:46:57 +0900

    2020.04.22 user관련 모듈 기능 구현 (구현 사항) user관련 기능을 구현함

--- server/components/users/usersController.js
+++ server/components/users/usersController.js
@@ -2,24 +2,45 @@ const express = require('express');
 
 const { tokenVerify } = require('../auth/tokenVerify');
 
+const usersService = require('./usersService');
+
 const router = express.Router();
 
-router.get('/', tokenVerify, (req, res) => {
 
+router.post('/', tokenVerify, async (req, res) => {
+  let user;
+  try {
+    const { email } = req.body;
+    const { password } = req.body;
+    user = await usersService.postUser(email, password);
+  } catch (err) {
+    console.log(err);
+  }
+  return res.json(user);
+});
+
+router.put('/', tokenVerify, async (req, res) => {
+  let user;
+  try {
+    const email = req.decoded.userId;
+    const { password } = req.body;
+    user = usersService.updateUser(email, password);
+  } catch (err) {
+    console.log(err);
+  }
+  return res.json(user);
 });
 
 
-router.post('/', tokenVerify, (req, res) => {
-
-});
-
-router.put('/', tokenVerify, (req, res) => {
-
-});
-
-
-router.delete('/', tokenVerify, (req, res) => {
-
+router.delete('/', tokenVerify, async (req, res) => {
+  let user;
+  try {
+    const email = req.decoded.userId;
+    user = await usersService.deleteUser(email);
+  } catch (err) {
+    console.log(err);
+  }
+  return res.json(user);
 });
 
 module.exports = router;
--- server/components/users/usersService.js
+++ server/components/users/usersService.js
@@ -1,15 +1,36 @@
-module.exports.getUser = {
+const passwordHash = require('password-hash');
 
+const usersDAL = require('./usersDAL');
+
+
+module.exports.postUser = async (email, password) => {
+  let user;
+  try {
+    const hashedPassword = passwordHash.generate(password);
+    user = await usersDAL.create(email, hashedPassword);
+  } catch (err) {
+    console.log(err);
+  }
+  return user;
 };
 
-module.exports.postUser = {
-
+module.exports.updateUser = async (email, password) => {
+  let user;
+  try {
+    const hashedPassword = passwordHash.generate(password);
+    user = await usersDAL.update(email, hashedPassword);
+  } catch (err) {
+    console.log(err);
+  }
+  return user;
 };
 
-module.exports.updateUser = {
-
-};
-
-module.exports.deleteUser = {
-
+module.exports.deleteUser = async (email) => {
+  let user;
+  try {
+    user = await usersDAL.delete(email);
+  } catch (err) {
+    console.log(err);
+  }
+  return user;
 };
