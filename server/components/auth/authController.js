3634d695db70f5194a541c7c597803467088001a
장규희 <jang-gyuhui@jang-gyuhuis-MacBook-Pro.local>
2020-04-24 20:53:33 +0900

    2020.04.24 인증모듈 구현 -구현중-

--- server/components/auth/authController.js
+++ server/components/auth/authController.js
@@ -1,8 +1,32 @@
 const express = require('express');
 
+const { verifyToken } = require('./verifyToken');
+
+const authService = require('./authService');
+
 const router = express.Router();
 
-router.get('/token', {
+router.post('/login', verifyToken, async (req, res) => {
+  let token;
+  try {
+    const { userId } = req.body;
+    const { password } = req.body;
+    token = authService.makeToken(userId);
+  } catch (err) {
+    console.log(err);
+  }
+  return res.json({ token });
 });
+
+router.posdt('/refresh', verifyToken, async (req, res) => {
+  let token;
+  try {
+    const { userId } = req.decoded;
+    token = authService.makeToken(userId);
+  } catch (err) {
+    console.log(err);
+  }
+  return token;
+})
 
 module.exports = router;
